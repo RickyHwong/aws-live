f3722e03a307782b034a050ae81dbefc1a9f63db
RickyHwong <141216079+RickyHwong@users.noreply.github.com>
2023-09-03 21:11:50 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+customhost = "employee.c03mgjrnetjv.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "rickyhwongyujun-employee"
+customregion = "us-east-1"
+
